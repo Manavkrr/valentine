c44ef81919528f15afc3d428709fe1eb10ae0c46
Manavkrr <manav050bandula@gmail.com>
2026-02-08 09:54:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ def home():
 
 @app.route("/yes")
 def yes():
-    return render_template("yes.html")
+    return render_template("Yes.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
